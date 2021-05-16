2d2124e61030d8ed87e1808bd3f872e697b88cd8
Kimbsy <lordkimber@gmail.com>
2021-05-16 14:09:01 +0100

    Integrate stockfish, move to fen-based board

---
+++ src/chess/stockfish.clj
@@ -0,0 +1,34 @@
+(ns chess.stockfish
+  (:require [chess.common :as c]
+            [clojure.java.shell :as shell]
+            [clojure.string :as s]))
+
+(defn extract-last-line
+  "The last line can be one of:
+    bestmove <move>
+    bestmove <move> ponder <other-move>"
+  [last-line]
+  (-> last-line
+      (s/split #" ")
+      second))
+
+(defn best-move
+  [fen & {:keys [move-time] :or {move-time 3000}}]
+  (let [fen-command  (str "position fen " fen)
+        move-command (str "go movetime " move-time)
+        input        (str fen-command "\nd\n" move-command "\n")]
+    (some-> (shell/sh "stockfish" :in input)
+            :out
+            s/split-lines
+            last
+            extract-last-line)))
+
+(defn apply-uci
+  [fen uci]
+  (let [input (str "position fen " fen " moves " uci "\nd\n")]
+    (some-> (shell/sh "stockfish" :in input)
+            :out
+            s/split-lines
+            (#(filter (fn [l] (re-matches #"^Fen:.*" l)) %))
+            first
+            (subs 5))))
